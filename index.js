7ab80acd367281c4c4a393fed513fcc5c7cc5936
jigneshkmistry <jignesh@trivediat.com>
2018-07-15 18:10:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+const App = () => (
+    <Text>Some Text</Text>
+);
+
+AppRegistry.registerComponent('test', () => App);
