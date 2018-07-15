ce8f69c3fc41cde6baa202e44b76d35e6733945b
jigneshkmistry <jignesh@trivediat.com>
2018-07-15 19:19:52 +0530

    added Header component

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-    <Text>Some Text</Text>
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
 );
 
 AppRegistry.registerComponent('test', () => App);
